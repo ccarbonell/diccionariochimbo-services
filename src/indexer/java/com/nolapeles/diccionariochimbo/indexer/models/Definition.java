2633e6dc1737e50e13378e596fd875309e8e5c1d
Angel Leon <gubatron@gmail.com>
2011-06-14 02:51:42 -0400

    Definition updateScore will only update the score for a Definition only after the expiration interval for the score is done. This will save unnecessary confirmations for the same statuses.

--- src/indexer/java/com/nolapeles/diccionariochimbo/indexer/models/Definition.java
+++ src/indexer/java/com/nolapeles/diccionariochimbo/indexer/models/Definition.java
@@ -6,6 +6,7 @@ import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
 import com.google.code.morphia.annotations.Indexed;
 import com.google.code.morphia.annotations.Reference;
+import com.nolapeles.diccionariochimbo.indexer.IndexerConstants;
 
 @Entity
 public class Definition {
@@ -20,13 +21,24 @@ public class Definition {
 	@Indexed
 	public long indexed_date;
 	public int score;
+	public long lastScoreUpdateTimestamp;
 	public int numRetweets;
 	public int numWins;
 	public int numFails;
 	
-	public int updateScore() {
-		score = numRetweets*Scores.RT_POINTS + numWins*Scores.WIN_POINTS + numFails*Scores.FAIL_POINTS;
-		return score;
+	/**
+	 * If the score has not been updated within the {@link IndexerConstants}.DEFINITION_SCORE_EXPIRATION_INTERVAL
+	 * it will calculate the score for this definition.
+	 * @return true if the score was updated.
+	 */
+	public boolean updateScore() {
+		/** We don't want to be updating scores unnecesarily. */
+		if (System.currentTimeMillis() - lastScoreUpdateTimestamp > IndexerConstants.DEFINITION_SCORE_EXPIRATION_INTERVAL) { 
+			score = numRetweets*Scores.RT_POINTS + numWins*Scores.WIN_POINTS + numFails*Scores.FAIL_POINTS;
+			lastScoreUpdateTimestamp = System.currentTimeMillis();
+			return true;
+		}
+		return false;
 	}
 	
 }
