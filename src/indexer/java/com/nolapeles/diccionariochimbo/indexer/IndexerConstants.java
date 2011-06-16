2583765fbc4d5a7b63324cd99fec2d84cfa49aca
Angel Leon <gubatron@gmail.com>
2011-06-16 01:23:01 -0400

    Forgot to commit this one. Class to keep constants that have something to do with the indexer.

---
+++ src/indexer/java/com/nolapeles/diccionariochimbo/indexer/IndexerConstants.java
@@ -0,0 +1,8 @@
+package com.nolapeles.diccionariochimbo.indexer;
+
+public class IndexerConstants {
+
+	/** Amount of time a Definition score will last before we recalculate. (1 HOUR)*/
+	public static final long DEFINITION_SCORE_EXPIRATION_INTERVAL = 3600000;
+
+}
